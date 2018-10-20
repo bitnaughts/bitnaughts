33199ac00286bc00963bf508eeda35d1c61bb137
Brian <brianhungerman@gmail.com>
2018-10-19 23:34:21 -0700

    Organizing Pt.2

---
+++ Assets/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+
+    int opening_speed = 0;
+    bool opened = false;
+
+    public GameObject TitleMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!opened && opening_speed > 0) TitleMenu.GetComponent<RectTransform>().Translate(new Vector2(0, opening_speed--));
+
+        if (opened && opening_speed > 0) TitleMenu.GetComponent<RectTransform>().Translate(new Vector2(0, -opening_speed--));
+    }
+
+    public void OpenTitleMenu()
+    {
+        opened = !opened;
+        opening_speed = 15;
+    }
+}
