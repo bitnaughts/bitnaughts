3a530d5ab79cd74d7955454248e91e8f0fead757
Brian <brianhungerman@gmail.com>
2018-12-26 13:32:01 -0800

    Zipping archived scripts to avoid namespace collisions

--- Assets/Deprecated_Scripts/ControllerHolder.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ControllerHolder : MonoBehaviour {
-    //  public R
-    public RuntimeAnimatorController controller;
-}
--- Assets/Deprecated_Scripts/ShipSize.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ShipSize : MonoBehaviour 
-{
-	public Vector2 size;
-}
