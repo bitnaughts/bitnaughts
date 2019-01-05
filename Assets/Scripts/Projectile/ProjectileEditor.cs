be7f8e270980c6c2b4e053779bdcc1b590b397a7
Brian <brianhungerman@gmail.com>
2019-01-05 15:35:24 -0800

    Adding projectile scripts

---
+++ Assets/Scripts/Projectile/ProjectileEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		this.transform.Translate (new Vector2 (0, Input.GetAxis ("Vertical") * .05f));
+		this.transform.Rotate (new Vector3 (0, 0, -Input.GetAxis ("Horizontal")));
+    }
+}
