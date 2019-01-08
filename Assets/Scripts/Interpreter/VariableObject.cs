83b09fab0bf225593635ce225d40fa4d0412345b
Brian <brianhungerman@gmail.com>
2019-01-08 15:47:11 -0800

    Moved VariableObject to own file

---
+++ Assets/Scripts/Interpreter/VariableObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class VariableObject {
+
+    public string type;
+    public string name;
+    /* For Primitive Data Types, value == value, otherwise value holds ToString() */
+    public string value;
+    VariableObject[] fields;
+
+    public VariableObject (string type) {
+        init (type, "", "");
+    }
+    public VariableObject (string type, string name) {
+        init (type, name, "");
+    }
+    public VariableObject (string type, string name, string value) {
+        init (type, name, value);
+    }
+    public void init (string type, string name, string value) {
+        this.type = type;
+        this.name = name;
+        this.value = value;
+
+        switch (type) {
+            case "Vector2":
+                fields = new VariableObject[] {
+                    new VariableObject ("float", "x", value.Split (',') [0]),
+                    new VariableObject ("float", "y", value.Split (',') [1])
+                };
+                break;
+            default:
+                fields = null;
+                break;
+        }
+    }
+    public override string ToString () {
+        string output = type + " " + name + " = " + value;
+        if (fields != null) {
+
+            for (int i = 0; i < fields.Length; i++) {
+                output += "-> -> Field(" + fields[i].ToString () + ")\n";
+            }
+        }
+        return output;
+    }
+}
