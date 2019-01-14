709c191a1e507ddcca273c947cd9f98c50137d68
Brian <brianhungerman@gmail.com>
2019-01-14 11:12:28 -0800

    Functioning sphere resolution indices

--- Assets/Scripts/GalaxyGeneration/GalaxyObject.cs
+++ Assets/Scripts/GalaxyGeneration/GalaxyObject.cs
@@ -1,43 +1,36 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class GalaxyObject
-{
-    public List<StarObject> stars = new List<StarObject>();
+public class GalaxyObject {
+    public List<StarObject> stars = new List<StarObject> ();
 
-
-    public void build(int seed, float size, float declineValue, float dispersion)
-    {
+    public void build (int seed, float size, float declineValue, float dispersion) {
         Random.seed = seed;
 
-        Vector2 systemLocation = new Vector2(0, 0);
+        Vector2 systemLocation = new Vector2 (0, 0);
         float tempSize = size;
-        
+
         //Add Stars
-        while(tempSize < size)
-        {
+        while (tempSize < size) {
             tempSize -= declineValue;
 
-            if(tempSize < 0)
-            {
+            if (tempSize < 0) {
                 tempSize += size;
-                systemLocation = new Vector2(Random.value * dispersion - (dispersion / 2), Random.value * dispersion - (dispersion / 2));
+                systemLocation = new Vector2 (Random.value * dispersion - (dispersion / 2), Random.value * dispersion - (dispersion / 2));
             }
 
-            Vector2 potentialLocation = new Vector2(Random.value * tempSize - (tempSize / 2) + systemLocation.x, Random.value * tempSize - (tempSize / 2) + systemLocation.y; 
+            Vector2 potentialLocation = new Vector2 (Random.value * tempSize - (tempSize / 2) + systemLocation.x, Random.value * tempSize - (tempSize / 2) + systemLocation.y);
             bool available = true;
-            for (int i = 0; i < stars.Count; i++)
-            {
-                if (Mathf.Abs(potentialLocation.x - stars[i].position.x) + Mathf.Abs(potentialLocation.y - stars[i].position.y) < .6f) //.06 is closest two stars can be to eachother (based on sprite size*)
+            for (int i = 0; i < stars.Count; i++) {
+                if (Mathf.Abs (potentialLocation.x - stars[i].position.x) + Mathf.Abs (potentialLocation.y - stars[i].position.y) < .6f) //.06 is closest two stars can be to eachother (based on sprite size*)
                 {
                     available = false;
                     break;
                 }
             }
-            if (available)
-            {
-                stars.Add(new StarObject(potentialLocation, seed));
+            if (available) {
+                stars.Add (new StarObject (potentialLocation, seed));
             }
         }
     }
--- Assets/Scripts/GalaxyGeneration/PlanetEditor.cs
+++ Assets/Scripts/GalaxyGeneration/PlanetEditor.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class PlanetEditor : MonoBehaviour {
     // Start is called before the first frame update
+    List<TriangleHolder> triangles = new List<TriangleHolder> ();
+    public GameObject marker;
     void Start () {
-
+        
+        print (CreateNSphere (1));
+        for (int i = 0; i < triangles.Count; i++) {
+            Instantiate(marker, triangles[i].p1, this.transform.rotation);
+        }
+          triangles = new List<TriangleHolder> ();
+        print (CreateNSphere (2));
+        for (int i = 0; i < triangles.Count; i++) {
+            triangles[i].p1.x += 5;
+            Instantiate(marker, triangles[i].p1, this.transform.rotation);
+        }
+         triangles = new List<TriangleHolder> ();
+        print (CreateNSphere (3));
+        for (int i = 0; i < triangles.Count; i++) {
+            triangles[i].p1.x += 10;
+            Instantiate(marker, triangles[i].p1, this.transform.rotation);
+        }
+        triangles = new List<TriangleHolder> ();
+        print (CreateNSphere (4));
+        for (int i = 0; i < triangles.Count; i++) {
+            triangles[i].p1.x += 15;
+            Instantiate(marker, triangles[i].p1, this.transform.rotation);
+        }
+        triangles = new List<TriangleHolder> ();
+        print (CreateNSphere (5));
+        for (int i = 0; i < triangles.Count; i++) {
+            triangles[i].p1.x += 20;
+            Instantiate(marker, triangles[i].p1, this.transform.rotation);
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +50,8 @@ public class PlanetEditor : MonoBehaviour {
      * Return the number of facets created.
      * The number of facets will be (4^iterations) * 8
      */
-    int CreateNSphere (FACET3 * f, int iterations) {
+    int CreateNSphere (int iterations) {
+
         int i, it;
         float a;
         Vector3[] p = { new Vector3 (0, 0, 1), new Vector3 (0, 0, -1), new Vector3 (-1, -1, 0), new Vector3 (1, -1, 0), new Vector3 (1, 1, 0), new Vector3 (-1, 1, 0) };
@@ -28,75 +59,62 @@ public class PlanetEditor : MonoBehaviour {
         int nt = 0, ntold;
 
         /* Create the level 0 object */
-        a = 1 / Mathf.Sqrt (2.0);
+        a = 1 / Mathf.Sqrt (2.0f);
         for (i = 0; i < 6; i++) {
             p[i].x *= a;
             p[i].y *= a;
         }
-        f[0].p1 = p[0];
-        f[0].p2 = p[3];
-        f[0].p3 = p[4];
-        f[1].p1 = p[0];
-        f[1].p2 = p[4];
-        f[1].p3 = p[5];
-        f[2].p1 = p[0];
-        f[2].p2 = p[5];
-        f[2].p3 = p[2];
-        f[3].p1 = p[0];
-        f[3].p2 = p[2];
-        f[3].p3 = p[3];
-        f[4].p1 = p[1];
-        f[4].p2 = p[4];
-        f[4].p3 = p[3];
-        f[5].p1 = p[1];
-        f[5].p2 = p[5];
-        f[5].p3 = p[4];
-        f[6].p1 = p[1];
-        f[6].p2 = p[2];
-        f[6].p3 = p[5];
-        f[7].p1 = p[1];
-        f[7].p2 = p[3];
-        f[7].p3 = p[2];
+        triangles.Add (new TriangleHolder (p[0], p[3], p[4]));
+        triangles.Add (new TriangleHolder (p[0], p[4], p[5]));
+        triangles.Add (new TriangleHolder (p[0], p[5], p[2]));
+        triangles.Add (new TriangleHolder (p[0], p[2], p[3]));
+        triangles.Add (new TriangleHolder (p[1], p[4], p[3]));
+        triangles.Add (new TriangleHolder (p[1], p[5], p[4]));
+        triangles.Add (new TriangleHolder (p[1], p[2], p[5]));
+        triangles.Add (new TriangleHolder (p[1], p[3], p[2]));
         nt = 8;
 
-        if (iterations < 1)
-            return (nt);
+        if (iterations < 1) return nt;
 
         /* Bisect each edge and move to the surface of a unit sphere */
         for (it = 0; it < iterations; it++) {
             ntold = nt;
             for (i = 0; i < ntold; i++) {
-                pa.x = (f[i].p1.x + f[i].p2.x) / 2;
-                pa.y = (f[i].p1.y + f[i].p2.y) / 2;
-                pa.z = (f[i].p1.z + f[i].p2.z) / 2;
-                pb.x = (f[i].p2.x + f[i].p3.x) / 2;
-                pb.y = (f[i].p2.y + f[i].p3.y) / 2;
-                pb.z = (f[i].p2.z + f[i].p3.z) / 2;
-                pc.x = (f[i].p3.x + f[i].p1.x) / 2;
-                pc.y = (f[i].p3.y + f[i].p1.y) / 2;
-                pc.z = (f[i].p3.z + f[i].p1.z) / 2;
-                Normalise ( & pa);
-                Normalise ( & pb);
-                Normalise ( & pc);
-                f[nt].p1 = f[i].p1;
-                f[nt].p2 = pa;
-                f[nt].p3 = pc;
-                nt++;
-                f[nt].p1 = pa;
-                f[nt].p2 = f[i].p2;
-                f[nt].p3 = pb;
-                nt++;
-                f[nt].p1 = pb;
-                f[nt].p2 = f[i].p3;
-                f[nt].p3 = pc;
-                nt++;
-                f[i].p1 = pa;
-                f[i].p2 = pb;
-                f[i].p3 = pc;
+                pa.x = (triangles[i].p1.x + triangles[i].p2.x) / 2;
+                pa.y = (triangles[i].p1.y + triangles[i].p2.y) / 2;
+                pa.z = (triangles[i].p1.z + triangles[i].p2.z) / 2;
+                pb.x = (triangles[i].p2.x + triangles[i].p3.x) / 2;
+                pb.y = (triangles[i].p2.y + triangles[i].p3.y) / 2;
+                pb.z = (triangles[i].p2.z + triangles[i].p3.z) / 2;
+                pc.x = (triangles[i].p3.x + triangles[i].p1.x) / 2;
+                pc.y = (triangles[i].p3.y + triangles[i].p1.y) / 2;
+                pc.z = (triangles[i].p3.z + triangles[i].p1.z) / 2;
+                pa = Vector3.Normalize (pa);
+                pb = Vector3.Normalize (pb);
+                pc = Vector3.Normalize (pc);
+                print (nt);
+
+                makeTriangle (nt++, new TriangleHolder (triangles[i].p1, pa, pc));
+                makeTriangle (nt++, new TriangleHolder (pa, triangles[i].p2, pb));
+                makeTriangle (nt++, new TriangleHolder (pb, triangles[i].p3, pc));
+                makeTriangle (i, new TriangleHolder (pa, pb, pc));
             }
         }
 
-        return (nt);
+        return nt;
+    }
+    public void makeTriangle (int index, TriangleHolder triangle) {
+        if (index >= triangles.Count) {
+            triangles.Add (triangle);
+        } else triangles[index] = triangle;
     }
 
+}
+public class TriangleHolder {
+    public Vector3 p1, p2, p3;
+    public TriangleHolder (Vector3 p1, Vector3 p2, Vector3 p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
 }
