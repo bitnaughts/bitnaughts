63e77db10ab8906432bf40a075334b06ebc42986
Brian <brianhungerman@gmail.com>
2019-01-22 10:10:19 -0800

    Adding perspective support for camera manager

--- Assets/Scripts/Scenes/CameraManager.cs
+++ Assets/Scripts/Scenes/CameraManager.cs
@@ -4,17 +4,55 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
 
-	// Use this for initialization
+	Camera camera;
+	Vector2 movement;
+
+	bool isPerspective = false;
+
 	void Start () {
-
+		Camera camera = this.GetComponent<Camera> ();
+		setCameraProperties ();
 	}
-
-	// Update is called once per frame
 	void Update () {
-		if (ShipManager.getSelectedShip () != null) {
+		updateCamera ();
+	}
+	void updateCamera () {
+		/* Get Movement Vector */
+		if (Referencer.shipManager.isShipSelected ()) movement = new Vector2 (-this.transform.position.x + Referencer.shipManager.getSelectedShipPosition ().x, -this.transform.position.y + Referencer.shipManager.getSelectedShipPosition ().y)
+		else movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
 
-			this.transform.position = new Vector3 (ShipManager.getSelectedShip ().ship.transform.position.x, ShipManager.getSelectedShip ().ship.transform.position.y, -20);
-			this.GetComponent<Camera> ().orthographicSize -= Input.GetAxis ("Mouse ScrollWheel");
+		/* Move Camera via Input */
+		if (movement.x != 0 || movement.y != 0) {
+			this.transform.Translate (new Vector3 (
+				movement.x * CameraProperties.SENSITIVITY * Time.deltaTime,
+				movement.y * CameraProperties.SENSITIVITY * Time.deltaTime,
+				0
+			));
+		}
+		/* Handle Zooming based on Camera Mode */
+		if (isPerspective) ? updatePerspectiveCameraZoom () : updateOrthographicCameraZoom ();
+	}
+	void updatePerspectiveCameraZoom () {
+		/* Zooming Camera */
+		if (Input.GetAxis ("Mouse ScrollWheel") != 0) {
+			this.transform.Translate (new Vector3 (0, 0 Input.GetAxis ("Mouse ScrollWheel") * CameraProperties.SENSITIVITY * Time.deltaTime * this.transform.position.y));
+			if (this.transform.position.y < CameraProperties.MIN_Y || this.transform.position.y > CameraProperties.MAX_Y) {
+				this.transform.position = new Vector3 (this.transform.position.x, Mathf.Clamp (this.transform.position.y, CameraProperties.MIN_Y, CameraProperties.MAX_Y), this.transform.position.z);
+			}
+		}
+	}
+	void updateOrthographicCameraZoom () {
+		// TODO: implement if desired
+	}
+	void switchCameraMode () {
+		isPerspective = !isPerspective;
+		setCameraProperties ();
+	}
+	void setCameraProperties () {
+		if (isPerspective) {
+			// TODO: reset properties
+		} else {
+			// TODO: reset properties
 		}
 	}
 }
---
+++ Assets/Scripts/Scenes/CameraProperties.cs
@@ -0,0 +1,13 @@
+﻿public static class CameraProperties {
+
+	const float SENSITIVITY = 10f;
+
+	const float MIN_Y = 2f,
+		MAX_Y = 50f;
+
+	const float SHIP_OFFSET_X = 1f,
+		SHIP_OFFSET_Y = 1f;
+
+	const float ANGLE_X = 10f,
+		ANGLE_Y = 10f;
+}
