16526e98d74ff3c36023ef2f8deeee58706fa024
Brian <brianhungerman@gmail.com>
2019-04-06 15:55:01 -0700

    ConsoleManager controlling ConsoleObjects

--- Assets/Scripts/Scenes/Referencer.cs
+++ Assets/Scripts/Scenes/Referencer.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 
 public static class Referencer {
-    public static ConsoleObject consoleManager;
+    public static ConsoleManager consoleManager;
     // public static PlotterManager plotterManager;
     public static Slider codeSpeedTester;
     public static ShipManager shipManager;
