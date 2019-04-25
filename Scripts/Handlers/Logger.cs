956fc5662685f7b1aee38ea5fa53c9e8c0b4976d
Brian <brianhungerman@gmail.com>
2019-04-25 15:55:37 -0700

    Disabling Logger, waiting for dynamic approach

--- Scripts/Handlers/Logger.cs
+++ Scripts/Handlers/Logger.cs
@@ -2,13 +2,22 @@
 using System.IO;
 
 class Logger {
+
     public static void Log (string logMessage) {
-        using (StreamWriter w = File.AppendText ("C:\\Users\\Mutilar\\Documents\\GitHub\\BitNaughtsUnity\\Assets\\bitnaughts\\Assets\\Scripts\\Handlers\\log.txt")) {
-            w.Write ("\r\nLog Entry : ");
-            w.WriteLine ($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
-            w.WriteLine ("  :");
-            w.WriteLine ($"  :{logMessage}");
-            w.WriteLine ("-------------------------------");
+
+        //if necessary to debug with this tool, be sure to update the file directory as appropriate
+        bool enabled = false;
+        string directory = "C:\\Users\\Mutilar\\Documents\\GitHub\\BitNaughtsUnity\\Assets\\bitnaughts\\Scripts\\Handlers";
+        string file_name = "log.txt";
+
+        if (enabled) {
+            using (StreamWriter w = File.AppendText (directory + "\\" + file_name)) {
+                w.Write ("\r\nLog Entry : ");
+                w.WriteLine ($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                w.WriteLine ("  :");
+                w.WriteLine ($"  :{logMessage}");
+                w.WriteLine ("-------------------------------");
+            }
         }
     }
 }
