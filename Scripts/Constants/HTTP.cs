72a2681c2e2ec4d8070f73eda5b6d5e29a84c9e4
Brian <brianhungerman@gmail.com>
2019-11-03 12:23:32 -0800

    Adding HTTP Constants

---
+++ Scripts/Constants/HTTP.cs
@@ -0,0 +1,21 @@
+public static class HTTP {
+    public const string GET = "get", POST = "post", DELETE = "delete", PUT = "put";
+    public static class Endpoints {
+        public const string GET = "get",
+            SET = "set",
+            UPDATE = "update",
+            RESET = "reset";
+
+        public static class Parameters {
+            public const string FLAG = "flag",
+                TYPE = "type",
+                ID = "id";
+            public static class Values {
+                public const string RESET = "reset",
+                    TYPE = "type",
+                    ID = "id";
+            }
+        }
+
+    }
+}
