fdab36df99b0b6860c147ff414f5b7bd3ac3236b
Brian <brianhungerman@gmail.com>
2019-12-10 18:00:56 -0800

    Reworked camera controller

--- Scripts/Controllers/CameraController.cs
+++ Scripts/Controllers/CameraController.cs
@@ -9,28 +9,35 @@ public class CameraController : MonoBehaviour {
 	Transform camera_transform;
 	Vector3 movement;
 
-	bool isPerspective = true;
+	// bool isPerspective = true;
 
 	void Start () {
-		camera_transform = this.transform.GetChild (0);
-		camera = camera_transform.GetComponent<Camera> ();
-		setCameraProperties ();
+		// camera_transform = this.transform.GetChild (0);
+		camera = this.GetComponent<Camera> ();
+		// setCameraProperties ();
 	}
 	void Update () {
 		/* Get Movement Vector */
 		// if (ShipManager.isShipSelected ()) movement = new Vector3 (-this.transform.position.x + (ShipManager.getSelectedShipPosition ().x - CameraProperties.OFFSET_X), Input.GetAxis ("Mouse ScrollWheel"), -this.transform.position.z + (ShipManager.getSelectedShipPosition ().z - CameraProperties.OFFSET_Z));
-		// else movement = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Mouse ScrollWheel"), Input.GetAxis ("Vertical"));
 
-		// /* Move Camera via Input */
-		// if (movement.x != 0 || movement.z != 0) {
-		// 	this.transform.Translate (new Vector3 (
-		// 		movement.x * CameraProperties.SENSITIVITY * Time.deltaTime,
-		// 		0,
-		// 		movement.z * CameraProperties.SENSITIVITY * Time.deltaTime
-		// 	));
-		// }
-		// /* Handle Zooming based on Camera Mode */
-		// if (movement.y != 0) {
+		float x = 0;
+		if (Input.GetKey("a")) x = -1;
+		if (Input.GetKey("d")) x = 1;
+		
+		float y = 0;
+		if (Input.GetKey("s")) y = -1;
+		if (Input.GetKey("w")) y = 1;
+
+		/* Move Camera via Input */
+		this.transform.Translate (
+			new Vector2 (
+				x * 10 * Time.deltaTime,
+				y * 10 * Time.deltaTime
+			)
+		);
+		/* Handle Zooming based on Camera Mode */
+		camera.orthographicSize += Input.GetAxis ("Mouse ScrollWheel") * -100 * Time.deltaTime;
+
 
 		// 	if (isPerspective) {
 		// 		this.transform.Translate (new Vector3 (0, movement.y * CameraProperties.SENSITIVITY * Time.deltaTime * this.transform.position.y, 0));
@@ -44,17 +51,17 @@ public class CameraController : MonoBehaviour {
 
 		// }
 	}
-	void switchCameraMode () {
-		isPerspective = !isPerspective;
-		setCameraProperties ();
-	}
-	void setCameraProperties () {
-		if (isPerspective) {
-			// TODO: reset properties
-			camera_transform.rotation = Quaternion.Euler(new Vector3(90 - CameraProperties.ANGLE_X, CameraProperties.ANGLE_Y,0));
+	// void switchCameraMode () {
+	// 	// isPerspective = !isPerspective;
+	// 	setCameraProperties ();
+	// }
+	// void setCameraProperties () {
+	// 	if (isPerspective) {
+	// 		// TODO: reset properties
+	// 		camera_transform.rotation = Quaternion.Euler(new Vector3(90 - CameraProperties.ANGLE_X, CameraProperties.ANGLE_Y,0));
 
-		} else {
-			// TODO: reset properties
-		}
-	}
+	// 	} else {
+	// 		// TODO: reset properties
+	// 	}
+	// }
 }
