8da81020893eae04ce11de81b72eba49f86b3799
Brian <brianhungerman@gmail.com>
2019-12-26 01:00:13 -0800

    PointHandler => PointF beefiness

--- Handlers/PointHandler.cs
+++ Handlers/PointHandler.cs
@@ -1,7 +1,40 @@
 using System;
+using UnityEngine;
 
-public static class PointHandler {
+#region Point-related Classes
+/* Holds relevant parameters for visualizing and calculating UI and World spatiotemporal (TM JV) values */
+#region TupleF Class
+public class TupleF {
+    public float x { get; set; } = 0f;
+    public float y { get; set; } = 0f;
+    public float z { get; set; } = 0f;
+    public TupleF (float x, float y) { this.x = x; this.y = y; this.z = 0; }
+    public TupleF (float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+    public override string ToString () {
+        return x + " " + y + " " + z;
+    }
+}
+#endregion
+#region PointF Class
+public class PointF : TupleF {
     public const float Rad2Deg = 57.2958f;
+    
+    public static readonly PointF zero;
+    
+    static PointF () {
+        zero = new PointF (0, 0, 0);
+    }
+
+    public PointF (float x, float y) : base (x, y) { }
+    public PointF (float x, float y, float z) : base (x, y, z) { }
+
+    public static PointF operator + (PointF left, PointF right) {
+        return new PointF (
+            left.x + right.x,
+            left.y + right.y,
+            left.z + right.z
+        );
+    }
     public static float GetDistance (PointF start, PointF end) {
         return (float) Math.Sqrt (
             Math.Pow ((end.x - start.x), 2) +
@@ -19,31 +52,11 @@ public static class PointHandler {
             (start.y - end.y) / (start.x - end.x)
         );
     }
-}
-#region Point-related Classes
-/* Holds relevant parameters for visualizing and calculating UI and World spatiotemporal (TM JV) values */
-#region TupleF Class
-public class TupleF {
-    public float x { get; set; } = 0f;
-    public float y { get; set; } = 0f;
-    public float z { get; set; } = 0f;
-    public TupleF (float x, float y) { this.x = x; this.y = y; this.z = 0; }
-    public TupleF (float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
-    public override string ToString () {
-        return x + " " + y + " " + z;
+    public static implicit operator Vector3 (PointF point) {
+        return new Vector3 (point.x, point.y, point.z);
     }
-}
-#endregion
-#region PointF Class
-public class PointF : TupleF {
-    public PointF (float x, float y) : base (x, y) { }
-    public PointF (float x, float y, float z) : base (x, y, z) { }
-    public static PointF operator + (PointF left, PointF right) {
-        return new PointF (
-            left.x + right.x,
-            left.y + right.y,
-            left.z + right.z
-        );
+    public static implicit operator PointF (Vector3 point) {
+        return new PointF (point.x, point.y, point.z);
     }
 }
 #endregion
@@ -51,6 +64,20 @@ public class PointF : TupleF {
 public class SizeF : TupleF {
     public SizeF (float x, float y) : base (x, y) { }
     public SizeF (float x, float y, float z) : base (x, y, z) { }
+    public static SizeF operator + (SizeF left, float right) {
+        return new SizeF (
+            left.x + right,
+            left.y + right,
+            left.z + right
+        );
+    }
+    public static SizeF operator - (SizeF left, float right) {
+        return new SizeF (
+            left.x - right,
+            left.y - right,
+            left.z - right
+        );
+    }
 }
 #endregion
 #region OrbitF Class
