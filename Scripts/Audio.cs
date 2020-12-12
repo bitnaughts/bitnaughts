57e73ddc44f3ad917d6b14141fdde9db6f21e8ce
Brian Hungerman <brianhungerman@gmail.com>
2020-12-12 15:12:11 -0800

    Cleaning

--- Scripts/Audio.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class Audio {
-    public const int COARSENESS = 6, //goes from 6 to 12
-        CHANNEL = 0; 
-    public const FFTWindow WINDOW = FFTWindow.Hamming;//Rectangular;
-    
-
-}
